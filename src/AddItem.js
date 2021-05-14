baf1220bb307eba4b7ee879f6b04cc61a29fe9b9
harry79ph <73724613+harry79ph@users.noreply.github.com>
2021-05-15 00:21:44 +0100

    Update AddItem.js

--- src/AddItem.js
+++ src/AddItem.js
@@ -4,14 +4,8 @@ const AddItem = (props) => {
 
     const onFormSubmit = (e) => {
         e.preventDefault();
-        if (e.target[0].value === '') {
-            alert('Ops! You forgot to add Title...');
-        } else if (e.target[1].value === '') {
-            alert('Ops! You forgot to add Content...');
-        } else {
-            props.onUserSubmit(e.target[0].value, e.target[1].value);
-            e.target.reset();
-        }
+        props.onUserSubmit(e.target[0].value, e.target[1].value);
+        e.target.reset();
     }
 
     return (
@@ -19,10 +13,10 @@ const AddItem = (props) => {
             <h1>Todo Organizer</h1>
             <form onSubmit={onFormSubmit} className="adder">
                 <div className="title">
-                    <input type="text" name="title" maxLength="28" placeholder="Add Title" />
+                    <input type="text" name="title" maxLength="28" placeholder="Add Title" required/>
                 </div>
                 <div className="content">
-                    <textarea name="content" rows="6" cols="20" placeholder="Add Content" maxLength="300"></textarea>
+                    <textarea name="content" rows="6" cols="20" placeholder="Add Content" maxLength="300" required></textarea>
                 </div>
                 <input className="btn" type="submit" value="Submit" />
             </form>
