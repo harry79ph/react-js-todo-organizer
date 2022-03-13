3f42591e918189da4dfd401dd5bbb2033d06777c
harry79 <serdare_79@hotmail.com>
2022-03-13 20:10:31 +0000

    submit button updated

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,13 +1,17 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 const AddItem = (props) => {
 
     const btnRef = useRef();
 
-    const onFormSubmit = e => {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const onFormSubmit = (e) => {
         e.preventDefault();
-        props.onUserSubmit(e.target[0].value, e.target[1].value);
-        e.target.reset();
+        props.onUserSubmit(title, content);
+        setTitle('');
+        setContent('');
     }
 
     const addToLocale = types => {// adds cando and todo items to local strorage
@@ -48,13 +52,13 @@ const AddItem = (props) => {
             <h1>Todo Organizer</h1>
             <form onSubmit={onFormSubmit} className="adder">
                 <div className="title">
-                    <input type="text" name="title" maxLength="28" placeholder="Add Title" required />
+                    <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)} maxLength="28" placeholder="Add Title"/>
                 </div>
                 <div className="content">
-                    <textarea name="content" rows="6" cols="20" placeholder="Add Content" maxLength="1000" required></textarea>
+                    <textarea name="content" value={content} onChange={(e) => setContent(e.target.value)} rows="6" cols="20" placeholder="Add Content" maxLength="1000"></textarea>
                 </div>
                 <div className="btn-wrapper">
-                    <input className="btn" type="submit" value="Submit" />
+                    <input className="btn" type="submit" value="Submit" disabled={title.length > 0 && content.length > 0 ? false : true}/>
                     <input className="btn" type="button" ref={btnRef} onClick={handleSave} value="Save" />
                 </div>
             </form>
